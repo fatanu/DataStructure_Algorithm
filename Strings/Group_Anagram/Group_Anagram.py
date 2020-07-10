a5ce3eca0950ff1b69600c1822cb354ec4c46837
Anuoluwapo Fatokun <fatokun.anu@gmail.com>
2020-07-10 00:28:56 -0400

    Group Anagram

---
+++ Strings/Group_Anagram/Group_Anagram.py
@@ -0,0 +1,25 @@
+"""
+Given an array of strings, group anagrams together
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+Return:
+[
+ ["ate", "eat", "tea"],
+ ["nat", "tan"]
+ ["bat"]
+]
+
+"""
+
+def grp_anagram(lst):
+    anagramMap = {}
+    for str in lst:
+        sortedStr = "".join(sorted(str))
+        if sortedStr not in anagramMap.keys():
+            anagramMap[sortedStr] = []
+        anagramMap[sortedStr].append(str)
+    return list(anagramMap.values())
+
+
+test = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(grp_anagram(test))
